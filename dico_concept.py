9d353c53426ed7af2b637978455cb4283c4ba952
djelines <139062149+djelines@users.noreply.github.com>
2023-07-28 01:41:03 +0200

    Delete dico_concept.py

--- dico_concept.py
+++
@@ -1,7 +0,0 @@
-ï»¿dico={}
-dico['parapluie']=[0,34,56]
-dico['musee']=[16, 54, 11]
-dico['cheval']=[6, 33, 27, 96, 112]
-dico['lion']=[6, 33, 45, 39]
-dico['Pere Noel']=[3, 19, 98, 105]
-dico['escargot']=[6, 32, 27, 82]Texte à écrire dans le fichierTexte à écrire dans le fichier
